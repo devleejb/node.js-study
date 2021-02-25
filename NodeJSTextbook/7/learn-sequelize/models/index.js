428cce3137708850b6216d3145018ae311324216
LeeJongBeom <devleejb@gmail.com>
2021-02-25 14:12:57 +0000

    2021-02-25

--- NodeJSTextbook/7/learn-sequelize/models/index.js
+++ NodeJSTextbook/7/learn-sequelize/models/index.js
@@ -13,9 +13,12 @@ db.sequelize = sequelize;
 db.User = User;
 db.Comment = Comment;
 
-User.init(sequelize);
+// 각각의 모델의 init 메소드 실행
+// 테이블이 모델로 연결
+User.init(sequelize); 
 Comment.init(sequelize);
 
+// 다른 테이블과의 관계 연결
 User.associate(db);
 Comment.associate(db);
 
