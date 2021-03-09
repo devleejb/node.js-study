fb79dcdb77732c80b2aed93bb4e06eb0748a107d
LeeJongBeom <devleejb@gmail.com>
2021-03-09 13:46:14 +0000

    2021-03-09

--- NodeJSTextbook/9/nodebird/app.js
+++ NodeJSTextbook/9/nodebird/app.js
@@ -5,11 +5,15 @@ const path = require('path');
 const session = require('express-session');
 const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
-
+const passport = require('passport');
 dotenv.config();
+
 const pageRouter = require('./routes/page');
+const { sequelize } = require('./models');
+const passportConfig = require('./passport');
 
 const app = express();
+passportConfig();
 
 app.set('port', process.env.PORT || 3000);
 app.set('view engine', 'html');
@@ -17,6 +21,26 @@ nunjucks.configure('views', {
 	express: app,
 	watch: true,
 });
+
+sequelize.sync({ force: false })
+	.then(() => {
+		console.log('db 연결 성공');
+	})
+	.catch((err) => {
+		console.error(err);
+	});
+
+app.use(session({
+	resave: false,
+	saveUninitialized: false,
+	secret: process.env.COOKIE_SECRET,
+	cookie: {
+		httpOnly: true,
+		secure: false,
+	},
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
